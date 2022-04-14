0bb0c0fc4618d65730e49cfddc3565c98a271a4d
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-04-14 22:24:49 +0200

    Added Rate Limit & API Cache

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express")
 const cors = require("cors")
+const rateLimit = require("express-rate-limit")
 require("dotenv").config()
 
 const port = process.env.PORT || 5000
 
 const app = express()
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter)
+app.set('trust proxy', 1)
 
 // app.use(cors)
 
