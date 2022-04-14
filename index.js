d970f4cdcfb72d33973e4bf693c675f1a55c44f7
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-04-14 22:53:09 +0200

    Added Public Assets

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ const rateLimit = require("express-rate-limit")
 require("dotenv").config()
 
 const port = process.env.PORT || 5000
-
 const app = express()
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 app.use(limiter)
 app.set('trust proxy', 1)
 
-// app.use(cors)
+app.use(express.static('public'))
+
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
+
+app.use(cors(corsOptions))
 
 app.use('/api', require("./routes"))
 
